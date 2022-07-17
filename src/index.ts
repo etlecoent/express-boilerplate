377867effccb8eba71f37a8bedccd40ac708255e
Etienne LC <etlecoent@gmail.com>
2022-07-17 15:12:27 -0400

    Add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
 import config from './config';
 
 const app = express();
+
+// Set appropriate headers for security
 app.use(helmet);
+// Remove x-powered-by header
+app.disable('x-powered-by');
+
+// Properly parse content
+app.use(bodyParser);
+
 app.listen(config.server.port, config.server.host, () => {});
