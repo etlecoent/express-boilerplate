f42c12a9bcd0a6e95ecbf649e31da932170c8573
Etienne LC <etlecoent@gmail.com>
2022-07-17 16:09:10 -0400

    Fix helmet & body-parser usages

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,23 @@ import config from './config';
 const app = express();
 
 // Set appropriate headers for security
-app.use(helmet);
+app.use(helmet());
 // Remove x-powered-by header
 app.disable('x-powered-by');
 
 // Log incoming requests
 app.use(morgan('common'));
-// Properly parse content
-app.use(bodyParser);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Handle errors passed to next function, needs to be last middleware
 app.use(errorMiddleware);
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
 
 app.listen(config.server.port, config.server.host, () => {
   console.log(`Server started on ${config.server.host}:${config.server.port}`);
