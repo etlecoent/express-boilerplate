d6966c25fa0b203e0fcd64fd3edaf59048504994
Etienne LC <etlecoent@gmail.com>
2022-07-17 15:05:16 -0400

    Add helmet middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import helmet from 'helmet';
 import config from './config';
 
 const app = express();
-
+app.use(helmet);
 app.listen(config.server.port, config.server.host, () => {});
